cdf9578eaa9b10b3797de8914bd5c0d65a479c24
Adam Neef <adneef@gmail.com>
2018-04-12 22:27:58 -0600

    commenting

--- REopt/app.js
+++ REopt/app.js
@@ -7,6 +7,7 @@ const margin = {top: 20, right: 20, bottom: 50, left: 150},
 x = d3.scaleLinear(),
 y = d3.scaleBand().padding(0.1)
 
+// create global variables to store our data and location
 let theData
 let where
 
@@ -66,38 +67,33 @@ const g = svg
 // create a months array, corresponding to the data we expect to receive
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec', 'Annual']
 
-  g
-  .append('g')
-  .attr('class', 'axis axis--x')
+// instantiate chart title, axes, axis titles, and the sun image on the left - values will be filled in later
+svg.append('text').attr('class', 'chartTitle')
 
-  g
-  .append('text')
-  .attr('class', 'label x-title')
+g.append('g').attr('class', 'axis axis--x')
+g.append('g').attr('class', 'axis axis--y')
 
-  g
-  .append('g')
-  .attr('class', 'axis axis--y')
+g.append('text').attr('class', 'label x-title')
+g.append('text').attr('class', 'y-title label')
 
-  g
-  .append('text')
-  .attr('class', 'y-title label')
+g.append('ellipse').attr('class', 'core')
+g.append('ellipse').attr('class', 'corona')
 
-  svg
-  .append('text')
-  .attr('class', 'chartTitle')
-
+// get our data and organize it
 const getData = async () => {
 
-  // get our data
+  // actually grab our data
   const result = await d3.json('./PVWattsData.json')
 
-  // pull out of the data the stuff we want
+  // grab city and state out of our data and process it to be the format we want
   let city = result.station_info.city,
   state = result.station_info.state
 
   city = city[0] + city.slice(1).toLowerCase()
 
   where = `${city}, ${state}`
+
+  // grab the solrad_monthly array from result and push solrad_annual on the end
   let solRadMonthlyArr = result.outputs.solrad_monthly
   solRadMonthlyArr.push(result.outputs.solrad_annual)
 
@@ -109,6 +105,7 @@ const getData = async () => {
     data.unshift({month: months[i], solRad: solRadMonthlyArr[i]})
   }
 
+  // set data to our global data variable
   theData = data
 
   // set the x and y domains based on our returned data
@@ -121,11 +118,10 @@ const getData = async () => {
 // create the function that will make our barchart (es6)
 const makeChart = () => {
 
+
   const bounds = svg.node().getBoundingClientRect(),
   width = bounds.width - margin.right - margin.left,
   height = bounds.height - margin.top - margin.bottom
-
-  console.log(bounds)
 
   x.range([0, width])
   y.rangeRound([height, 0])
@@ -136,6 +132,11 @@ const makeChart = () => {
   .attr('transform', `translate(0, ${height})`)
   .call(d3.axisBottom(x))
 
+  // label the y-axis
+  g
+  .select('.axis--y')
+  .call(d3.axisLeft(y))
+
   // title the x-axis
   g
   .select('.x-title')
@@ -143,11 +144,6 @@ const makeChart = () => {
   .attr('x', width/2)
   .attr('text-anchor', 'middle')
   .text('kWh/m2/day')
-
-  // label the y-axis
-  g
-  .select('.axis--y')
-  .call(d3.axisLeft(y))
 
   // title the y-axis
   g
@@ -185,8 +181,10 @@ const makeChart = () => {
   .selectAll('.barLabel')
   .data(theData)
 
+  // remove extra bars on window resize
   barLabels.exit().remove()
 
+  // add bar
   barLabels
   .enter()
   .append('text')
@@ -205,17 +203,16 @@ const makeChart = () => {
   .text(d => +d.solRad.toFixed(3))
 
   // add a sun to the left side of our svg, inner and outer components
-  const corona = svg
-  .append('ellipse')
+  svg
+  .select('.corona')
   .data(theData)
-  .attr('class', 'corona')
-  .attr('cx', 0)
+  .attr('cx', -margin.left)
   .attr('cy', y('Aug'))
   .attr('ry', d => y(d.month)/3)
   .attr('rx', +margin.left * 0.6)
 
-  const core = svg
-  .append('ellipse')
+  svg
+  .select('.core')
   .data(theData)
   .attr('class', 'core')
   .attr('cx', 0)
